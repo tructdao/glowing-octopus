d7772a22ea9612e32bdd5447d732a2e632a73ad6
tructdao <tdao@stuy.edu>
2017-04-21 10:35:47 -0400

    constructors

--- Ticket.java
+++ Ticket.java
@@ -3,5 +3,21 @@ public class Ticket{
     private int _vipLevel, _ID;
     private String _descrip, _name, _solutionDescrip;
     private boolean _solved;
+
+    public Ticket(){
+	_vipLevel = 0;
+	_ID = 0000;
+	_descrip = "";
+	_name = "";
+	_solutionDescrip = "";
+	_solved = false;
+    }
+
+    public Ticket(int vip, String descrip, String name){
+	this();
+	_vipLevel = vip;
+	_descrip = descrip;
+	_name = name;
+    }
     
 }
