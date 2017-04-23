d49e1cc964750e19365c5b8820254a8fb42e85f5
Truc <tdao@stuy.edu>
2017-04-23 16:23:12 -0400

    broken computer

--- Ticket.java
+++ Ticket.java
@@ -1,3 +1,5 @@
+import cs1.Keyboard;
+
 public class Ticket implements Comparable{
 
     private int _vipLevel, _ID;
@@ -59,4 +61,57 @@ public class Ticket implements Comparable{
 	}
     }
 
+    public void solveIssue(){
+	System.out.println("Welcome to the Help Desk!\n" +
+			   "Thank you for your patience.\n" +
+			   "Please respond to the following questions with " +
+			   "Y or N. Thanks for your cooperation!");
+	if (_descrip.equals("A")){
+	    brokenComputer();
+	}
+	if (_descrip.equals("B")){
+	}
+	_solved = true;
+    }
+
+    public void brokenComputer(){
+	System.out.println("Have you tried turning it on and off yet?");
+	System.out.println("Response: ");
+	String response = Keyboard.readString().toUpperCase();
+	if (response.equals("Y")){
+	    System.out.println("Is your computer in one piece?");
+	    System.out.println("Response: ");
+	    response = Keyboard.readString().toUpperCase();
+	    if (response.equals("N")){
+		System.out.println("You need a new computer then. " +
+				   "Thank you for calling!");
+	    }
+	    else{
+		System.out.println("Is your computer connected to a power " +
+				   "source?");
+		System.out.println("Does it have any battery left?");
+		System.out.println("Response: ");
+		response = Keyboard.readString().toUpperCase();
+		if (response.equals("N")){
+		    System.out.println("Plug it in and come back later if it " +
+				       "still isn't working. Thanks for " +
+				       "calling!");
+		}
+		else{
+		}
+	    }
+	}
+	else{
+	    System.out.println("Please do so.");
+	    System.out.println("Did it work?");
+	    System.out.println("Response: ");
+	    response = Keyboard.readString().toUpperCase();
+	    if (response.equals("Y")){
+		System.out.println("Great to hear! Have a good day");
+	    }
+	    else{
+		brokenComputer();
+	    }
+	}
+    }
 }
