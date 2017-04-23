2e8c55731d9222f8ba8f578fa3da91a192fdcd77
Bayan Berri <bberri@stuy.edu>
2017-04-22 21:35:50 -0400

    added main method for helpdesk

--- HelpDesk.java
+++ HelpDesk.java
@@ -15,13 +15,16 @@ public class HelpDesk{
     public void newTicket(){
 	System.out.println("Hello, We look forward to solving your problem.\n to get started can you tell us your name?");
 	String nombre= Keyboard.readString();
-	System.out.println("ok, thank you" + nombre+ ". Can you describe your problem for me?");
+	System.out.println("ok, thank you " + nombre+ ". Can you describe your problem for me?");
 	String problem= Keyboard.readString();
 	int importance= (int) (Math.random()*25);
+	System.out.println("ok I understand your problem, here's your ticket number " + numPpl +". We will help you shortly, please hold. :)");
 	_tickets.add( new Ticket(importance, problem, nombre,numPpl));
 	numPpl=numPpl+1;
     }
 
     public static void main(String[] args){
+	HelpDesk hd= new HelpDesk();
+	hd.newTicket();
     }
 }
