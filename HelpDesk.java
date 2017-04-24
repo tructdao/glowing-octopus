91d0e9086f5c167b6a628a2c691afd5cf0291d6b
ArifRoktim <aroktim@stuy.edu>
2017-04-23 21:29:46 -0400

    Add main loop for HelpDesk

--- HelpDesk.java
+++ HelpDesk.java
@@ -4,41 +4,60 @@ public class HelpDesk{
 
     private ArrayPriorityQueue<Ticket> _tickets;
     private int numPpl;
-    private int solved;
     
     public HelpDesk(){
 	_tickets = new ArrayPriorityQueue<Ticket>();
 	numPpl=0;
-	solved=0;
     }
 
     public void newTicket(){
-	System.out.println("Hello, We look forward to solving your problem.\n"
+	System.out.println("Hello, we look forward to solving your problem.\n"
 			   + "To get started can you tell us your name?");
-	String nombre = Keyboard.readString();
-	System.out.println("OK, thank you " + nombre + ". " +
-			   "Can you describe your problem for me?");
+	String name = Keyboard.readString();
+	System.out.println("OK, thank you " + name + ". ");
 	String problem = issue();
 	int importance = (int) (Math.random()*25);
 	System.out.println("OK I understand your problem, here's your ticket " +
-			   "number " + numPpl +".\n" +
-			   "We will help you shortly, please hold. :)");
-	_tickets.add( new Ticket(importance, problem, nombre,numPpl) );
+			   "number: " + numPpl +".\n" +
+			   "Please check back later. :)");
+	_tickets.add( new Ticket(importance, problem, name,numPpl) );
 	numPpl=numPpl+1;
     }
 
     public static String issue(){
+    System.out.println("Can you describe your problem?");
+    String ret = Keyboard.readString();
+    System.out.println("Interesting... What have you tried so far to fix it?" );
+    ret += Keyboard.readString();
+    /*
 	System.out.println("Do you have a:");
 	System.out.println("\tA) broken computer?");
 	System.out.println("\tB) Internet issues?");
 	System.out.print("\nResponse(A or B): ");
 	String ret = Keyboard.readString().toUpperCase();
 	System.out.println();
+    */
 	return ret;
     }
 
     public static void main(String[] args){
-	HelpDesk hd= new HelpDesk();
-	hd.newTicket();
+        HelpDesk hd= new HelpDesk();
+        boolean needHelp = true;
+        while( needHelp ){
+            System.out.println("Hello, this is tech support. Do you wish to create a ticket or look at an old one?");
+            System.out.println("A) Create Ticket");
+            System.out.println("B) Look at Ticket");
+            System.out.println("C) Exit");
+            String ans = Keyboard.readString().toUpperCase();
+            if( ans.equals("A") ){
+                hd.newTicket();
+            } else if( ans.equals("B") ){
+                //Add case where person views ticket and it is dequeued
+            } else if( ans.equals("C") ){
+                needHelp = false;
+            } else {
+                System.out.println("Input invalid");
+            }
+        }
     }
 }
